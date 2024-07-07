1e02d0160ced58f3fc03f4e1f295a5b5e200271b
silverblade34 <mpacheco.tacay@gmail.com>
2024-07-06 21:41:15 -0500

    Create: QR web site, validaciones ok

--- lib/app/controllers/qrwebsite_controller.dart
+++ lib/app/controllers/qrwebsite_controller.dart
@@ -1,6 +1,24 @@
 import 'package:get/get.dart';
 
 class QrWebsiteController extends GetxController {
+  var url = ''.obs;
+  var qrData = ''.obs;
 
+  void setUrl(String value) {
+    url.value = value;
+  }
 
+  bool _isValidURL(String url) {
+    const urlPattern = r'^(https?:\/\/)?([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$';
+    final result = RegExp(urlPattern).hasMatch(url);
+    return result;
+  }
+
+  void generateQr() {
+    if (_isValidURL(url.value)) {
+      qrData.value = url.value;
+    } else {
+      Get.snackbar("Error", "Por favor, ingrese un enlace válido.");
+    }
+  }
 }
--- lib/app/ui/pages/qrwebsite/qrwebsite_page.dart
+++ lib/app/ui/pages/qrwebsite/qrwebsite_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_yanapay_qr/app/controllers/qrwebsite_controller.dart';
 import 'package:app_yanapay_qr/app/ui/utils/style_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:get/get.dart';
 
 class QrWebsitePage extends GetView<QrWebsiteController> {
@@ -17,67 +18,86 @@ class QrWebsitePage extends GetView<QrWebsiteController> {
         backgroundColor: PRIMARY,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/qr-example.png",
-                  width: 250,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text(
-                    "Escribe tu URL Aquí",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Ingrese el link',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(15),
-                        backgroundColor: SECONDARY,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              8.0), // Cambia el valor de 8.0 por el radio deseado
+                  Obx(() {
+                    if (controller.qrData.isNotEmpty) {
+                      return Center(
+                        child: QrImageView(
+                          data: controller.qrData.value,
+                          version: QrVersions.auto,
+                          size: 200.0,
                         ),
-                      ),
-                      child: const Text(
-                        "Generar QR",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
+                      );
+                    } else {
+                      return Image.asset(
+                        "assets/images/qr-example.png",
+                        width: 250,
+                      );
+                    }
+                  }),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Escribe tu URL Aquí",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'Ingrese el link',
+                      ),
+                      onChanged: (value) {
+                        controller.setUrl(value);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.generateQr();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(15),
+                          backgroundColor: SECONDARY,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                8.0), // Cambia el valor de 8.0 por el radio deseado
+                          ),
+                        ),
+                        child: const Text(
+                          "Generar QR",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
